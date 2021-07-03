2248cc7c26fccadaf49e57df434da36cec5f37b5
LiorReznik <liorrezn@gmail.com>
2021-07-04 00:41:08 +0300

    add DELETE endpoint.

--- src/main/java/com/example/letscookit/controller/RecipeController.java
+++ src/main/java/com/example/letscookit/controller/RecipeController.java
@@ -13,28 +13,34 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/recipe")
 public class RecipeController {
     private final Map<Integer, Recipe> recipes = new HashMap<>();
 
-    @PostMapping("/recipe/new")
+    @PostMapping("/new")
     public Map<String, Integer> setRecipe(@RequestBody Recipe recipe) {
         Integer id = recipe.getID();
         this.recipes.put(id, recipe);
         return Map.of("id", id);
     }
 
-    @GetMapping("/recipe/{id}")
+    @GetMapping("/{id}")
     public Map<String, ?> getRecipe(@PathVariable int id) {
         Recipe r = this.recipes.get(id);
         if (r == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                    String.format("Recipe with %d id does not exists", id));
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         return Map.of("name", r.getName(),
                 "description", r.getDescription(),
                 "ingredients", r.getIngredients(),
                 "directions", r.getDirections()
         );
+    }
+    @DeleteMapping("/{id}")
+    public void deleteRecipe(@PathVariable int id) {
+        if (!this.recipes.containsKey(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        this.recipes.remove(id);
     }
 }
